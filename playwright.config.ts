21372c6dc5a083f01704e173ab9adb95631ca083
James Chanler <jchanler@gmail.com>
2025-08-12 01:05:53 -0400

    add ctrf report to github actions

--- playwright.config.ts
+++ playwright.config.ts
@@ -9,7 +9,9 @@ export default defineConfig({
   forbidOnly: !!process.env.CI,
   retries: process.env.CI ? 2 : 0,
   workers: process.env.CI ? 1 : undefined,
-  reporter: process.env.CI ? [['github'], ['html']] : 'html',
+  reporter: process.env.CI ? 
+    [['github'], ['html'], ['playwright-ctrf-json-reporter', { outputFile: 'ctrf/ctrf-report.json' }]] : 
+    [['html'], ['playwright-ctrf-json-reporter', { outputFile: 'ctrf/ctrf-report.json' }]],
   use: {
     baseURL: 'https://www.saucedemo.com',
     trace: 'on-first-retry',
