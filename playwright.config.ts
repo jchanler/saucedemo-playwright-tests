14ceeb0a3e147cdc0d576e89e8d9e82f180fbbce
James Chanler <jchanler@gmail.com>
2025-08-12 01:14:16 -0400

    update ctrf path

--- playwright.config.ts
+++ playwright.config.ts
@@ -10,8 +10,8 @@ export default defineConfig({
   retries: process.env.CI ? 2 : 0,
   workers: process.env.CI ? 1 : undefined,
   reporter: process.env.CI ? 
-    [['github'], ['html'], ['playwright-ctrf-json-reporter', { outputFile: 'ctrf/ctrf-report.json' }]] : 
-    [['html'], ['playwright-ctrf-json-reporter', { outputFile: 'ctrf/ctrf-report.json' }]],
+    [['github'], ['html'], ['playwright-ctrf-json-reporter']] : 
+    [['html'], ['playwright-ctrf-json-reporter']],
   use: {
     baseURL: 'https://www.saucedemo.com',
     trace: 'on-first-retry',
